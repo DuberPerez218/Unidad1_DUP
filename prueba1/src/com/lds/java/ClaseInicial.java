063b7466ce525e6c80a6282fbb0084005c7e1a5b
duberney.perez <duberney.perez@PROYECTOS-LMR.LINEADATASCAN.local>
2017-12-02 13:01:15 -0500

    Actualizacion inicial

---
+++ prueba1/src/com/lds/java/ClaseInicial.java
@@ -0,0 +1,10 @@
+package com.lds.java;
+
+public class ClaseInicial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("Hola Mundo");
+	}
+
+}
